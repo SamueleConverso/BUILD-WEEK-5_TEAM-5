79bebddda7c9d922013ab8bfd693340ac5b85a9b
Samuele Converso <samu.converso@gmail.com>
2025-04-02 09:43:58 +0200

    First commit frontend

---
+++ frontend/clinica-veterinaria/src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+}
+
+export default App;
