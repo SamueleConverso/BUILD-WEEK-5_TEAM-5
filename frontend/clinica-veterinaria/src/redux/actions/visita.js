634a7bfedda984ecc7b0108b136f346ae1098bdd
Samuele Converso <samu.converso@gmail.com>
2025-04-04 12:51:51 +0200

    .

--- frontend/clinica-veterinaria/src/redux/actions/visita.js
+++ frontend/clinica-veterinaria/src/redux/actions/visita.js
@@ -65,6 +65,34 @@ export const getVisite = () => {
   };
 };
 
+export const getVisitaById = (visitaId) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://localhost:7138/api/Visita/" + visitaId,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        dispatch({
+          type: "GET_VISITA_BY_ID",
+          payload: data.visita,
+        });
+      } else {
+        throw new Error("Errore nella response di getVisitaById");
+      }
+    } catch (error) {
+      console.error("ERRORE FETCH:" + error);
+    }
+  };
+};
+
 export const deleteVisita = (visitaId) => {
   return async (dispatch) => {
     try {
